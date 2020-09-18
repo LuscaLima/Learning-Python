ec710c28482bc663fe7c762139dedb5aeb5155f9
Lusca Lima <lusca.lima@outlook.com>
2020-09-17 22:23:01 -0300

    exer 006 lesson 07

---
+++ lesson-07/006.py
@@ -0,0 +1,10 @@
+# calculate the amount of paint needed to paint a wall
+
+width = float(input('Width: '))
+height = float(input('Height: '))
+
+print('\n1 liter of paint paints 2m²\n')
+
+area = width * height
+print('Area: {}'.format(area))
+print('Required amount of ink: {} liters'.format(area / (2*2)))
