6792da91b8c30289927940067fcbe36d818eeb4c
Lusca Lima <lusca.lima@outlook.com>
2020-09-17 21:38:01 -0300

    exer 001 lesson 07

---
+++ lesson-07/001.py
@@ -0,0 +1,5 @@
+# read a number and show your successor and predecessor
+
+number = int(input('A number: '))
+
+print('The number: {}\nSuccessor: {}\nPredecessor: {}'.format(number, number + 1, number - 1))
