2eee36df4c9ca895754d147e10ed2f63281a196b
Lusca Lima <lusca.lima@outlook.com>
2020-09-17 22:22:53 -0300

    exer 005 lesson 07

---
+++ lesson-07/005.py
@@ -0,0 +1,14 @@
+# read a number and show your table (no loop)
+
+number = int(input('Number: '))
+
+print('The number: {}\n'.format(number))
+print('{} * 1 = {:>3}'.format(number, number * 1))
+print('{} * 2 = {:>3}'.format(number, number * 2))
+print('{} * 3 = {:>3}'.format(number, number * 3))
+print('{} * 4 = {:>3}'.format(number, number * 4))
+print('{} * 5 = {:>3}'.format(number, number * 5))
+print('{} * 6 = {:>3}'.format(number, number * 6))
+print('{} * 7 = {:>3}'.format(number, number * 7))
+print('{} * 8 = {:>3}'.format(number, number * 8))
+print('{} * 9 = {:>3}'.format(number, number * 9))
