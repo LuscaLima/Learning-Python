39d11411f07feecd04e096c0b0c068474540759e
Lusca Lima <lusca.lima@outlook.com>
2020-09-17 22:22:46 -0300

    exer 004 lesson 07

---
+++ lesson-07/004.py
@@ -0,0 +1,6 @@
+# read a value in meters and show the amount of cement and millimeters
+
+meters = float(input('Meters: '))
+
+print('\nThe value: {}\n\nCements: {}\nMilimeters: {}'
+  .format(meters, meters * 100, meters * 100 * 10))
