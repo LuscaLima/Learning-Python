d74a705344c09ba96ef07a0a1b6b6727e74f3ba2
Lusca Lima <lusca.lima@outlook.com>
2020-09-17 21:42:58 -0300

    exer 002 lesson 07

---
+++ lesson-07/002.py
@@ -0,0 +1,6 @@
+# read a number and show its double, triple and square root
+
+number = int(input('Number: '))
+
+print('The number: {}\nDouble: {}\nTriple: {}\nSquare root: {}'
+  .format(number, number * 2, number * 3, number ** (1/2)))
